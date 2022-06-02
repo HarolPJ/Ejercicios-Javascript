de38cee1810d0bfc5c40430e128c603119cb7145
HarolPJ <99227654+HarolPJ@users.noreply.github.com>
2022-06-01 20:16:19 -0500

    Add files via upload

---
+++ Ejercicio1/ejercicio.js
@@ -0,0 +1,6 @@
+
+
+let nombre = prompt('Ingresar nombre porfavor')
+let edad = parseInt(prompt('Ingresar edad porfavor'))
+
+confirm(`Hola señ@r ${nombre} tienes actualmente ${edad} años, estaras proximo a cumplir ${age+1} años`);
